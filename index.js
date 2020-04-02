98b08f56273728af7263a28be39aa785039b7457
Denny Koh <denny.koh.kaichao@gmail.com>
2020-04-02 18:19:25 +0800

    first deliverables done

---
+++ index.js
@@ -0,0 +1,11 @@
+const fetch = require("./index2.js");
+
+console.log(fetch.hexToRgb(process.argv[3]));
+
+// console.log(
+//   fetch.rbgToHex(
+//     parseInt(process.argv[3]),
+//     parseInt(process.argv[4]),
+//     parseInt(process.argv[5])
+//   )
+// );
